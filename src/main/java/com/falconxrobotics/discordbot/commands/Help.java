c80d7acdc3632530d2ebd4d449c13ec392158524
Ray Z <raybipse@gmail.com>
2020-03-13 19:29:44 -0700

    Redid Help.

--- src/main/java/com/falconxrobotics/discordbot/commands/Help.java
+++ src/main/java/com/falconxrobotics/discordbot/commands/Help.java
@@ -1,5 +1,7 @@
 package com.falconxrobotics.discordbot.commands;
 
+import java.util.Optional;
+
 import com.github.raybipse.components.Command;
 import com.github.raybipse.components.CommandGroup;
 import com.github.raybipse.core.BotConfiguration;
@@ -17,9 +19,83 @@ public class Help extends Command {
 
     public Help() {
         super("Help", "help");
-        setPrefix("help");
         setDescription("Returns details and general information about the bot.");
-        setSyntax("");
+        setSyntax("[optional: command] OR [command group] [command]");
+        addExamples("");
+    }
+
+    private void onNoArgument(MessageReceivedEvent event) {
+        String[] allCommandGroupPrefixes = BotConfiguration.getAllCommandGroups().stream().map(CommandGroup::getPrefix)
+                .toArray(String[]::new);
+        String[] allStandaloneCommandPrefixes = BotConfiguration.getAllStandaloneCommands().stream()
+                .map(Command::getPrefix).toArray(String[]::new);
+
+        EmbedBuilder builder = new EmbedBuilder().setTitle("Help")
+                .setDescription("Peter Kim Jr., or Falkim Bot, is a Discord Bot created by FalconX Robotics.")
+                .setColor(BotConfiguration.getPromptColor())
+                .addField("Structure", "Commands can be stand alone or can be grouped into a \"``CommandGroup``\". "
+                        + "Every command in a command group have their own \"``help``\" command embeded in them. "
+                        + "To see the description and usage of those commands, type ``"
+                        + BotConfiguration.getBotPrefix() + "[command group prefix] help [specific command]``. "
+                        + "To see the description of a command group, type ``" + BotConfiguration.getBotPrefix()
+                        + "[command group prefix] help``.", false)
+                .addField("Arguments",
+                        "Command arguments are separated by spaces. E.g., \"``/command argument1 argument2``\". "
+                                + "If an argument requires spaces in it, wrap it around quotation marks. E.g., "
+                                + "\"``/command \"this is one argument\" argument 2``\". "
+                                + "Add a backslash if an argument requires quotations in it. You must type a double backslash because Discord "
+                                + "automatically deletes single backslashs when the message is sent.",
+                        false)
+                .addField("Bot prefix", MarkdownUtil.monospace(BotConfiguration.getBotPrefix()), false);
+        if (allCommandGroupPrefixes.length > 0) {
+            builder.addField("All command group prefixes", "``" + String.join("``, ``", allCommandGroupPrefixes) + "``",
+                    false);
+        } else {
+            builder.addField("All command group prefixes", "No visible command groups.", false);
+        }
+        if (allStandaloneCommandPrefixes.length > 0) {
+            builder.addField("All standalone command prefixes",
+                    "``" + String.join("``, ``", allStandaloneCommandPrefixes) + "``", false);
+        } else {
+            builder.addField("All standalone command prefixes", "No visible commands.", false);
+        }
+        builder.addField("Source code", "[GitHub](https://github.com/RayBipse/falkim-bot)", false);
+        builder.setFooter("Note: Some commands/command groups are hidden.");
+
+        event.getChannel().sendMessage(builder.build()).queue();
+    }
+
+    private void onCommandHelp(MessageReceivedEvent event, String prefix) {
+        Optional<Command> optional = BotConfiguration.getAllStandaloneCommands().stream()
+                .filter(e -> e.getPrefix().equals(prefix)).findFirst();
+        if (optional.isPresent()) {
+            event.getChannel().sendMessage(optional.get().getEmbedInfo().build()).queue();
+        } else {
+            event.getChannel()
+                    .sendMessage(getEmbedInvalidParameterError("Command Not Found")
+                            .addField("Note",
+                                    "If you want see the help command for a Command Group, use "
+                                            + BotConfiguration.getBotPrefix() + "[command group prefix] help",
+                                    false)
+                            .build())
+                    .queue();
+        }
+    }
+
+    private void onCommandGroupHelp(MessageReceivedEvent event, String groupPrefix, String commandPrefix) {
+        Optional<CommandGroup> commandGroupOptional = BotConfiguration.getAllCommandGroups().stream()
+                .filter(e -> e.getPrefix().equals(groupPrefix)).findFirst();
+        if (commandGroupOptional.isPresent() && commandGroupOptional.get().getChildren() != null) {
+            Optional<Command> commandOptional = commandGroupOptional.get().getChildren().stream()
+                .filter(e -> e.getPrefix().equals(commandPrefix)).findFirst();
+            if (commandOptional.isPresent()) {
+                event.getChannel().sendMessage(commandOptional.get().getEmbedInfo().build()).queue();
+            } else {
+                event.getChannel().sendMessage(getEmbedInvalidParameterError("Command Not Found Within Command Group").build()).queue();
+            }
+        } else {
+            event.getChannel().sendMessage(getEmbedInvalidParameterError("Command Group Not Found").build()).queue();
+        }
     }
 
     @Override
@@ -30,54 +106,15 @@ public class Help extends Command {
         if (!getInputValidity(messageContent))
             return;
 
-        // CommandGroup[] allCommandGroups = Bot.getAllCommandGroups();
-        // String[] allCommandGroupPrefixes = new String[allCommandGroups.length];
-        // for (int i = 0; i < allCommandGroups.length; i++) {
-        //     allCommandGroupPrefixes[i] = allCommandGroups[i].getPrefix();
-        // }
-
-        String[] allCommandGroupPrefixes = BotConfiguration.getAllCommandGroups().stream().map(CommandGroup::getPrefix).toArray(String[]::new);
-
-        // Command[] allStandaloneCommands = BotConfiguration.getAllStandaloneCommands();
-        // String[] allStandaloneCommandPrefixes = new String[allStandaloneCommands.length];
-        // for (int i = 0; i < allStandaloneCommands.length; i++) {
-        //     allStandaloneCommandPrefixes[i] = allStandaloneCommands[i].getPrefix();
-        // }
-
-        String[] allStandaloneCommandPrefixes = BotConfiguration.getAllStandaloneCommands().stream().map(Command::getPrefix).toArray(String[]::new);
-
-        EmbedBuilder builder = new EmbedBuilder()
-                .setTitle("Help")
-                .setDescription("Peter Kim Jr., or Falkim Bot, is a Discord Bot created by FalconX Robotics.")
-                .setColor(BotConfiguration.getPromptColor())
-                .addField("Structure", 
-                        "Commands can be stand alone or can be grouped into a \"``CommandGroup``\". "
-                        + "Every command in a command group have their own \"``help``\" command embeded in them. "
-                        + "To see the description and usage of those commands, type ``"
-                        + BotConfiguration.getBotPrefix() + "[command group prefix] help [specific command]``. "
-                        + "To see the description of a command group, type ``" + BotConfiguration.getBotPrefix()
-                        + "[command group prefix] help``.", false)
-                .addField("Arguments", 
-                        "Command arguments are separated by spaces. E.g., \"``/command argument1 argument2``\". "
-                        + "If an argument requires spaces in it, wrap it around quotation marks. E.g., "
-                        + "\"``/command \"this is one argument\" argument 2``\". "
-                        + "Add a backslash if an argument requires quotations in it. You must type a double backslash because Discord "
-                        + "automatically deletes single backslashs when the message is sent.", false)
-                .addField("Bot prefix", MarkdownUtil.monospace(BotConfiguration.getBotPrefix()), false);
-        if (allCommandGroupPrefixes.length > 0) {
-            builder.addField("All command group prefixes", "``"+String.join("``, ``", allCommandGroupPrefixes)+"``", false);
+        messageContent = trimInputBeginning(messageContent);
+        String[] arguments = splitUserInput(messageContent);
+        if (arguments.length == 0) {
+            onNoArgument(event);
+        } else if (arguments.length == 1) {
+            onCommandHelp(event, arguments[0]);
         } else {
-            builder.addField("All command group prefixes", "No visible command groups.", false);
+            onCommandGroupHelp(event, arguments[0], arguments[1]);
         }
-        if (allStandaloneCommandPrefixes.length > 0) {
-            builder.addField("All standalone command prefixes", "``"+String.join("``, ``", allStandaloneCommandPrefixes)+"``", false);
-        } else {
-            builder.addField("All standalone command prefixes", "No visible commands.", false);
-        }
-        builder.addField("Source code", "[GitHub](https://github.com/RayBipse/falkim-bot)", false);
-        builder.setFooter("Note: Some commands/command groups are hidden.");
-        
-        event.getChannel().sendMessage(builder.build()).queue();
     }
 
 }
