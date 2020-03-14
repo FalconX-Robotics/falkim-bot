416d0cf311153c470365ffdb74ccbfe3cae8fccd
Ray Z <raybipse@gmail.com>
2020-03-13 19:30:56 -0700

    New command group.

---
+++ src/main/java/com/falconxrobotics/discordbot/commands/eval/Evaluate.java
@@ -0,0 +1,24 @@
+package com.falconxrobotics.discordbot.commands.eval;
+
+import com.github.raybipse.components.CommandGroup;
+
+public class Evaluate extends CommandGroup {
+    private static Evaluate instance;
+    private JS js = new JS();
+
+    public Evaluate() {
+        super("Evaluate", "eval");
+        setDescription("Commands that are used to evaluate code.");
+        addChildren(js, new Help());
+    }
+
+    /**
+     * @return the sole instance of the class
+     */
+    public static Evaluate getInstance() {
+        if (instance == null) {
+            instance = new Evaluate();
+        }
+        return instance;
+    }
+}
